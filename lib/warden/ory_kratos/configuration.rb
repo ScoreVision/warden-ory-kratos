e6ac9298c466a1e6b135af1b84c2872847a59dca
Zach Norris <znorris@gmail.com>
2022-08-31 19:25:40 -0700

    add logger to configuration

--- lib/warden/ory_kratos/configuration.rb
+++ lib/warden/ory_kratos/configuration.rb
@@ -1,15 +1,16 @@
+
 module Warden
   module OryKratos
     class Configuration
       # <https://github.com/feministy/lizabinante.com/blob/stable/source/2016-01-30-creating-a-configurable-ruby-gem.markdown>
-      attr_accessor :kratos_external_api, :kratos_internal_api, :kratos_ui, :kratos_proxy_jwks
+      attr_accessor :kratos_external_api, :kratos_internal_api, :kratos_ui, :kratos_proxy_jwks, :logger
 
       def initialize
         @kratos_external_api  = nil
         @kratos_internal_api  = nil
         @kratos_ui            = nil
         @kratos_proxy_jwks    = nil
-        # TODO: Add a logger to the config?
+        @logger               = nil
       end
 
       def self.kratos_backend_api
